09dc71b408026a96efa5b6491f989a9287995c85
Nak <khat.ratanak@cloudnet.com.kh>
2018-05-29 15:44:01 +0700

    Nak upload system nanas k

--- help/add_update_file.sql
+++ help/add_update_file.sql
@@ -11,13 +11,17 @@ ADD COLUMN `expiry`  date NULL AFTER `wpiece`,
 ADD COLUMN `expiry_id`  int(11) NULL AFTER `expiry`;
 
 /* 28-05-2018 By Chanthy */
-ALTER TABLE `iclouderp_v3_5_nanas_k`.`erp_companies` 
-ADD COLUMN `plate_number` varchar(55) NULL AFTER `public_charge_id`;
-ADD COLUMN `plate_number_2` varchar(55) NULL AFTER `plate_number`;
-ADD COLUMN `plate_number_3` varchar(55) NULL AFTER `plate_number_2`;
-ADD COLUMN `plate_number_4` varchar(55) NULL AFTER `plate_number_3`;
-ADD COLUMN `plate_number_5` varchar(55) NULL AFTER `plate_number_4`;
+ALTER TABLE `erp_companies`
+  ADD COLUMN `plate_number` varchar(55) NULL,
+  ADD COLUMN `plate_number_2` varchar(55) NULL AFTER `plate_number`,
+  ADD COLUMN `plate_number_3` varchar(55) NULL AFTER `plate_number_2`,
+  ADD COLUMN `plate_number_4` varchar(55) NULL AFTER `plate_number_3`,
+  ADD COLUMN `plate_number_5` varchar(55) NULL AFTER `plate_number_4`;
 
 /* 29-05-2018 By Chanthy */
 ALTER TABLE `iclouderp_v3_5_nanas_k`.`erp_sales` 
 ADD COLUMN `plate_number` varchar(55) NULL AFTER `start_date`;
+
+/* 29-05-2018 By Nak */
+ALTER TABLE `erp_suspended_items`
+  ADD COLUMN `quantity_balance`  decimal(15,4) NULL AFTER `unit_price`;
