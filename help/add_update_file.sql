3e129c45677c1898197b344f3e7950b4e226e001
Nak <khat.ratanak@cloudnet.com.kh>
2018-05-30 15:13:18 +0700

    Nak upload system nanas k

--- help/add_update_file.sql
+++ help/add_update_file.sql
@@ -19,7 +19,7 @@ ALTER TABLE `erp_companies`
   ADD COLUMN `plate_number_5` varchar(55) NULL AFTER `plate_number_4`;
 
 /* 29-05-2018 By Chanthy */
-ALTER TABLE `iclouderp_v3_5_nanas_k`.`erp_sales` 
+ALTER TABLE `erp_sales`
 ADD COLUMN `plate_number` varchar(55) NULL AFTER `start_date`;
 
 /* 29-05-2018 By Nak */
@@ -27,5 +27,18 @@ ALTER TABLE `erp_suspended_items`
   ADD COLUMN `quantity_balance`  decimal(15,4) NULL AFTER `unit_price`;
   
 /* 30-05-2018 By Chanthy */
-  ALTER TABLE `iclouderp_v3_5_nanas_k`.`erp_suspended_bills` 
+  ALTER TABLE `erp_suspended_bills`
 ADD COLUMN `plate_number` varchar(55) NULL AFTER `suspend_name`;
+
+/* 28-05-2018 By Channa */
+DROP TABLE IF EXISTS `erp_gift_card_logs`;
+CREATE TABLE `erp_gift_card_logs` (
+  `id` int(11) NOT NULL,
+  `gift_card_id` int(11) DEFAULT NULL,
+  `date` datetime DEFAULT NULL,
+  `transaction_type` varchar(255) DEFAULT NULL,
+  `amount` double DEFAULT NULL,
+  `sale_id` int(11) DEFAULT NULL,
+  `created_by` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
